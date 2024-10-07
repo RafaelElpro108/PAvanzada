a1e2b492dba5584fd290ea68e1ba41adf9ae13a0
RafaelElpro108 <rafaelmartinezvillanueva@gmail.com>
2024-10-07 16:50:59 -0600

    Add files via upload

---
+++ Parcial2/ejercicio5-ps.js
@@ -0,0 +1,21 @@
+const api="https://pokeapi.co/api/v2/pokemon/223";
+const contenedor=document.getElementById("contenedor");
+//fetch consulta a una url mediante los verbos de http
+async function consultarPokemon()
+{
+    let consulta= await fetch(api)
+    let datos= await consulta.json();
+    dibujarPokemon(datos);
+}
+
+function dibujarPokemon(datosPokemon)
+
+
+{
+let rutaImagen=datosPokemon.sprites.other.home.front_default;
+let pokemon=`<h1> ${datosPokemon.name}</h1>
+<img src="${rutaImagen}"/>
+<h1>${datosPokemon.types[0].type.name}</h1>`;
+contenedor.innerHTML = pokemon;
+}
+consultarPokemon();
